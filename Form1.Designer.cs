10bac19bc5093c23c428ad0ae560cc391d489f79
mad-develop <sebastian.geisenhainer@keller-und-kollegen.de>
2023-12-15 10:47:38 +0100

    Code commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace YT_Player
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.webView = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(616, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(635, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 24);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // webView
+            // 
+            this.webView.AllowExternalDrop = true;
+            this.webView.CreationProperties = null;
+            this.webView.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView.Location = new System.Drawing.Point(13, 39);
+            this.webView.Name = "webView";
+            this.webView.Size = new System.Drawing.Size(775, 399);
+            this.webView.TabIndex = 3;
+            this.webView.ZoomFactor = 1D;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.webView);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+
+namespace YT_Player
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            await InitializeAsync();
+
+            // load HTML template to browser component
+            this.webView.NavigateToString(this.getYtHtml());
+
+        }
+
+        private async Task InitializeAsync()
+        {
+            // Initialize webview2 component and add message receiver event
+            await this.webView.EnsureCoreWebView2Async(null);
+            this.webView.CoreWebView2.WebMessageReceived += MessageReceived;
+        }
+
+        async void loadVideo(string youtubeId)
+        {
+            // Load video
+            await this.webView.CoreWebView2.ExecuteScriptAsync("createPlayer('" + youtubeId + "');");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Parse Uri from TextBox and load the video
+            Uri uri = new Uri(this.textBox1.Text);
+            string ytVideoId =  HttpUtility.ParseQueryString(uri.Query).Get("v");
+
+            this.loadVideo(ytVideoId);
+        }
+
+        private void MessageReceived(object sender, CoreWebView2WebMessageReceivedEventArgs args)
+        {
+            String content = args.TryGetWebMessageAsString();
+            MessageBox.Show(content);
+        }
+
+        private string getYtHtml()
+        {
+            // simple html template for YT JS api implementation
+            string html = @"<html>
+            <head><meta content='IE=Edge' http-equiv='X-UA-Compatible'/></head>
+            <body>
+	            <div id='player'></div>
+	            <script src='http://www.youtube.com/player_api'></script>
+	            <script>
+		            // create youtube player
+		            var player;
+		            var apiReady;
+
+		            // autoplay video
+		            function onPlayerReady(event) {
+			            event.target.playVideo();
+		            }
+
+		            // when video ends
+		            function onPlayerStateChange(event) {
+			            if (event.data === 0) {
+				            window.chrome.webview.postMessage('video play done');
+			            }
+		            }
+
+		            function onYouTubePlayerAPIReady() {
+			            apiReady = true;
+		            }
+
+		            function createPlayer(videoId) {
+			            if (!apiReady) {
+				            return;
+			            }
+
+			            if (player) {
+				            player.destroy();
+				            player = null;
+			            }
+			
+			            player = new YT.Player('player', {
+				            width: '640',
+				            height: '390',
+				            videoId: videoId,
+				            events: {
+					            onReady: onPlayerReady,
+					            onStateChange: onPlayerStateChange
+				            }
+			            });
+		            }
+	            </script>
+            </body>
+            </html>";
+
+            return html;
+        }
+    }
+}
